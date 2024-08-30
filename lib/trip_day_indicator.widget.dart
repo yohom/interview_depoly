cd855c67a277642a786ff03bd1bea40e02557262
yohom <yohombao@qq.com>
2024-08-30 23:33:41 +0800

    enhance: 顶部滚动

--- lib/trip_day_indicator.widget.dart
+++ lib/trip_day_indicator.widget.dart
@@ -1,39 +1,51 @@
+import 'dart:math';
+
 import 'package:decorated_flutter/decorated_flutter.dart';
 import 'package:flutter/material.dart';
 
 class TripDayIndicator extends StatelessWidget {
-  const TripDayIndicator({super.key});
+  const TripDayIndicator({
+    super.key,
+    required this.totalDaysTextSize,
+    required this.totalDaysHeight,
+  });
+
+  final double totalDaysTextSize, totalDaysHeight;
 
   @override
   Widget build(BuildContext context) {
     return DecoratedRow(
       itemSpacing: 8,
       scrollable: true,
-      height: 56,
+      height: 48 - max(0, totalDaysTextSize - totalDaysHeight),
       padding: const EdgeInsets.symmetric(horizontal: 12),
-      sliver: true,
+      margin: const EdgeInsets.only(top: 12),
       children: [
         DecoratedColumn(
           padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           decoration: BoxDecoration(
-            color: Colors.grey.shade400,
+            color: Colors.grey,
             borderRadius: BorderRadius.circular(12),
           ),
-          children: const [
-            Text('总览'),
-            Text('共6天'),
+          textStyle: const TextStyle(height: 1),
+          children: [
+            const Text('总览'),
+            DecoratedText('共6天', height: totalDaysHeight),
           ],
         ),
         for (int i = 0; i < 5; i++)
           DecoratedColumn(
             padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
             decoration: BoxDecoration(
-              color: Colors.grey.shade400,
+              color: Colors.grey.shade300,
               borderRadius: BorderRadius.circular(12),
             ),
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            textStyle: const TextStyle(height: 1),
             children: [
               Text('第 $i 天'),
-              const Text('共6天'),
+              DecoratedText('共6天', height: totalDaysHeight),
             ],
           ),
       ],
